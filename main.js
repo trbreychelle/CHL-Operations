0caf884ac2bddfc743b9b0d058bf9b274bc85984
trbreychelle <trb.reychelle@gmail.com>
2026-03-25 16:30:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -776,7 +776,7 @@ if (supaClient) {
     supaClient.from('client_package_allocation_view').select('*'),
     supaClient.from('profiles').select('*'),
     supaClient.from('agent_current_rate_view').select('*'),
-    supaClient.from('payroll_weekly_fact_view').select('*')
+    supaClient.from('agent_weekly_payroll_view').select('*')
   ]);
 
   supaLeads = lRes.data || [];
